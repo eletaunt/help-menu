afffe8f0b0ba461281206ee51aa6759ac35eb421
dyxh <david.huangg@gmail.com>
2015-06-01 13:46:26 -0500

    added menuresultquery

---
+++ app/src/main/java/edu/uchicago/cs234/spr15/limt/helpmenuapp/MenuResultQuery.java
@@ -0,0 +1,35 @@
+package edu.uchicago.cs234.spr15.limt.helpmenuapp;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class MenuResultQuery extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_result_query);
+
+        Bundle hashmap = this.getIntent().getExtras();
+        String query = hashmap.getString("query");
+
+        HelpMenuItem item = new HelpMenuItem(query);
+
+        TextView name = (TextView) this.findViewById(R.id.name);
+        name.setText(query);
+
+        item.execute();
+    }
+}
