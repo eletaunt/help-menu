756b35d3c5d60bbf7703c9edd6e41af7a3fadf8c
dyxh <david.huangg@gmail.com>
2015-06-01 15:31:06 -0500

    things

--- app/src/main/java/edu/uchicago/cs234/spr15/limt/helpmenuapp/HelpMenuItem.java
+++ app/src/main/java/edu/uchicago/cs234/spr15/limt/helpmenuapp/HelpMenuItem.java
@@ -1,8 +1,12 @@
 package edu.uchicago.cs234.spr15.limt.helpmenuapp;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.*;
@@ -10,28 +14,122 @@ import java.io.*;
 
 public class HelpMenuItem extends AsyncTask<String, Void, JSONObject>
 {
+    private String _apiKey = "4vjBKxjJZ3ps7TQ7djXHcHpoUyEOqLdL9Dja6QYR";
     private String _name;
+    private MenuResultQuery _act;
 
-    public HelpMenuItem(String name)
+
+    public HelpMenuItem(MenuResultQuery act, String name)
     {
         _name = name;
+        _act = act;
     }
 
     @Override
     protected JSONObject doInBackground(String... strings)
     {
         String query  = _name;
-        String apiKey = "4vjBKxjJZ3ps7TQ7djXHcHpoUyEOqLdL9Dja6QYR";
         String rawURL = "http://api.nal.usda.gov/usda/ndb/search/" +
                         "?format=" +    "json" +
                         "&q=" +         "banana" +
                         "&sort=" +      "r" +
                         "&max=" +       "1" +
                         "&offset=" +    "0" +
-                        "&api_key=" +   apiKey;
+                        "&api_key=" +   _apiKey;
 
-        String rawJSONString = "";
-        JSONObject json = new JSONObject();
+        String rawJSONString = getRawJSONString(rawURL);
+
+        Log.v("asdoifjaoiwjfawef--->", rawJSONString);
+
+        JSONObject j = null;
+        try
+        {
+            j = getNDBDescription(new JSONObject(rawJSONString));
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+        return j;
+//
+//        try
+//        {
+//            JSONObject json = new JSONObject(rawJSONString);
+//        }
+//        catch (JSONException e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        JSONObject json = new JSONObject();
+//        try
+//        {
+//            URL url = new URL(rawURL);
+//            URLConnection c = url.openConnection();
+//            BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+//
+//            String line;
+//
+//            while ((line = in.readLine()) != null)
+//            {
+//                rawJSONString += line;
+//            }
+//            in.close();
+//
+//            json = new JSONObject(rawJSONString);
+//            json = new JSONObject(json.getString("list"));
+//            JSONArray j = new JSONArray(json.getString("item"));
+//            json = j.getJSONObject(0);
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        return json;
+    }
+
+    private JSONObject getNDBDescription(JSONObject json)
+    {
+        String ndbno = "";
+        try
+        {
+            JSONObject jObject = new JSONObject(json.getString("list"));
+            JSONArray jArray = new JSONArray(jObject.getString("item"));
+            jObject = jArray.getJSONObject(0);
+            ndbno = jObject.getString("ndbno");
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+        String rawURL = "http://api.nal.usda.gov/usda/ndb/reports/" +
+                        "?ndbno=" +     ndbno +
+                        "&type=" +      "f" +
+                        "&format=" +    "json" +
+                        "&api_key=" +   _apiKey;
+
+        String rawJSONString = getRawJSONString(rawURL);
+
+        JSONObject j = null;
+        try
+        {
+            j = new JSONObject(rawJSONString);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+        return j;
+    }
+
+    private String getRawJSONString(String rawURL)
+    {
+        String result = "";
+
         try
         {
             URL url = new URL(rawURL);
@@ -42,19 +140,32 @@ public class HelpMenuItem extends AsyncTask<String, Void, JSONObject>
 
             while ((line = in.readLine()) != null)
             {
-                rawJSONString += line;
+                result += line;
             }
-            Log.v("THING ---->", "\n\n\n" + rawJSONString + "\n\n\n");
             in.close();
-
-            json = new JSONObject(rawJSONString);
-            Log.v("------->", json.getString("list"));
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
 
-        return json;
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject j)
+    {
+        TextView name = (TextView) _act.findViewById(R.id.name);
+        TextView random = (TextView) _act.findViewById(R.id.random);
+        name.setText(_name);
+        try
+        {
+            random.setText(j.toString(4));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            random.setText("Unfortunately, we could not find any details for '" + _name + "'.");
+        }
     }
 }
--- app/src/main/java/edu/uchicago/cs234/spr15/limt/helpmenuapp/MenuResultQuery.java
+++ app/src/main/java/edu/uchicago/cs234/spr15/limt/helpmenuapp/MenuResultQuery.java
@@ -1,12 +1,15 @@
 package edu.uchicago.cs234.spr15.limt.helpmenuapp;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -25,7 +28,7 @@ public class MenuResultQuery extends Activity
         Bundle hashmap = this.getIntent().getExtras();
         String query = hashmap.getString("query");
 
-        HelpMenuItem item = new HelpMenuItem(query);
+        HelpMenuItem item = new HelpMenuItem(this, query);
 
         TextView name = (TextView) this.findViewById(R.id.name);
         name.setText(query);
